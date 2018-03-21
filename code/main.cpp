e9c831043696a08522120e9944902c73f0e873ff
Draetus <mauricio199836@hotmail.com>
2018-03-21 16:36:10 -0300

    Main using and showing all outputs in a more fancy way

--- code/main.cpp
+++ code/main.cpp
@@ -14,11 +14,96 @@ int main(int argc, char** argv)
     std::vector<std::vector<float> > featVec;
 
     Descriptor desc;
-    desc.run(LBP_image, featVec, 3, 3);
-    
+    desc.run(LBP_image, featVec);
 
-/*    for (int i=0; i<featVec[0].size(); i++)
+/*
+    for (int i=0; i<featVec.size(); i++)
     {
-        std::cout << (int)featVec[2][i] << " " ;
-    }*/
+        for (int j=0; j<featVec[i].size(); j++)
+        {
+            std::cout << (int)featVec[i][j] << "\n";
+        }
+    }
+*/
+
+    for (int j=0; j<featVec[0].size(); j++)
+        {
+            std::cout << "Media 3x3: " << (int)featVec[0][j] << "\n";
+        }
+    for (int j=0; j<featVec[1].size(); j++)
+        {
+            std::cout << "Desvio Padrao 3x3: " << (int)featVec[1][j] << "\n";
+        }
+    for (int j=0; j<featVec[2].size(); j++)
+        {
+            std::cout << "Assimetria 3x3: " << (int)featVec[2][j] << "\n";
+        }
+    for (int j=0; j<featVec[3].size(); j++)
+        {
+            std::cout << "Curtose 3x3: " << (int)featVec[3][j] << "\n";
+        }
+    for (int j=0; j<featVec[4].size(); j++)
+        {
+            std::cout << "Energia 3x3: " << (int)featVec[4][j] << "\n";
+        }
+    for (int j=0; j<featVec[5].size(); j++)
+        {
+            std::cout << "Media 5x5: " << (int)featVec[5][j] << "\n";
+        }
+    for (int j=0; j<featVec[6].size(); j++)
+        {
+            std::cout << "Desvio Padrao 5x5: " << (int)featVec[6][j] << "\n";
+        }
+    for (int j=0; j<featVec[7].size(); j++)
+        {
+            std::cout << "Assimetria 5x5: " << (int)featVec[7][j] << "\n";
+        }
+    for (int j=0; j<featVec[8].size(); j++)
+        {
+            std::cout << "Curtose 5x5: " << (int)featVec[8][j] << "\n";
+        }
+    for (int j=0; j<featVec[9].size(); j++)
+        {
+            std::cout << "Energia 5x5: " << (int)featVec[9][j] << "\n";
+        }
+    for (int j=0; j<featVec[10].size(); j++)
+        {
+            std::cout << "Media 7x7: " << (int)featVec[10][j] << "\n";
+        }
+    for (int j=0; j<featVec[11].size(); j++)
+        {
+            std::cout << "Desvio Padrao 7x7: " << (int)featVec[11][j] << "\n";
+        }
+    for (int j=0; j<featVec[12].size(); j++)
+        {
+            std::cout << "Assimetria 7x7: " << (int)featVec[12][j] << "\n";
+        }
+    for (int j=0; j<featVec[13].size(); j++)
+        {
+            std::cout << "Curtose 7x7: " << (int)featVec[13][j] << "\n";
+        }
+    for (int j=0; j<featVec[14].size(); j++)
+        {
+            std::cout << "Energia 7x7: " << (int)featVec[14][j] << "\n";
+        }
+    for (int j=0; j<featVec[15].size(); j++)
+        {
+            std::cout << "Media 9x9: " << (int)featVec[15][j] << "\n";
+        }
+    for (int j=0; j<featVec[16].size(); j++)
+        {
+            std::cout << "Desvio Padrao 9x9: " << (int)featVec[16][j] << "\n";
+        }
+    for (int j=0; j<featVec[17].size(); j++)
+        {
+            std::cout << "Assimetria 9x9: " << (int)featVec[17][j] << "\n";
+        }
+    for (int j=0; j<featVec[18].size(); j++)
+        {
+            std::cout << "Curtose 9x9: " << (int)featVec[18][j] << "\n";
+        }
+    for (int j=0; j<featVec[19].size(); j++)
+        {
+            std::cout << "Energia 9x9: " << (int)featVec[19][j] << "\n";
+        }
 }
